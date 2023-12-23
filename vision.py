1aaea8f3b124265676f5952fbf2d369951c2b64a
Yuma Morimoto <kanten520212@gmail.com>
2023-12-23 19:49:10 +0900

    Check the output prompt when JSONDecodeError occurs.

--- vision.py
+++ vision.py
@@ -52,6 +52,7 @@ def get_actions(screenshot, objective):
         json_response = json.loads(response.choices[0].message.content)
     except json.JSONDecodeError:
         print("Error: Invalid JSON response")
+        print(response.choices[0].message.content)
         cleaned_response = openai.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
